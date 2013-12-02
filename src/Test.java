39e2ee023deb8fb07e28b5b1a59440f676bb60b4
EmilGeorgiev <emogeorgiev88@gmail.com>
2013-12-02 10:31:32 +0200

    //1

---
+++ src/Test.java
@@ -0,0 +1,10 @@
+/**
+ * Created with IntelliJ IDEA.
+ * User: clouway
+ * Date: 12/2/13
+ * Time: 10:30 AM
+ * To change this template use File | Settings | File Templates.
+ */
+public class Test {
+    //1
+}
